3de42d86f168654c85a35b05aa32b27e64f5ade5
Steve Hobbs <elkdanger@gmail.com>
2016-04-08 14:22:50 +0100

    Initial commit of website

---
+++ server.js
@@ -0,0 +1,33 @@
+'use strict'
+
+let express = require('express')
+let app = express()
+let http = require('http').Server(app)
+
+let io = require('socket.io')(http)
+
+app.use(express.static('public'))
+
+http.listen(3000, () => console.log('Listening on port 3000'))
+
+var users = 0;
+
+io.on('connection', socket => {
+    
+    console.log('A user connected')
+    
+    users++
+    
+    io.emit('users changed', users)
+    
+    socket.on('msg', msg => {
+        io.emit('msg', msg)
+    })
+    
+    socket.on('disconnect', () => {
+      console.log('user disconnected')
+      users--  
+      io.emit('users changed', users)
+    })
+    
+})
